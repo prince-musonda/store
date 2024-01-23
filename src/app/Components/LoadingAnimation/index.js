0f17657425db9da1842b34f6943b5cba84d6eafe
Prince Musonda <princemusonda5840@gmail.com>
2024-01-23 12:23:43 +0200

    --

--- src/app/Components/LoadingAnimation/index.js
+++ src/app/Components/LoadingAnimation/index.js
@@ -4,7 +4,7 @@ import LoadingSpinnerComponent from "react-spinners-components";
 export default function LoadingAnimation() {
   return (
     <LoadingSpinnerComponent
-      type={"Eater"}
+      type={"Discuss"}
       colors={["#5489d0", "#3d7dd5"]}
       size="30px"
     />
