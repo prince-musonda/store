34abcfdc1c9422fc7968dc761ac296efee7e60f6
Prince Musonda <princemusonda5840@gmail.com>
2024-02-07 23:28:26 +0200

    added better error handling

--- src/app/products/productId/[productId]/page.js
+++ src/app/products/productId/[productId]/page.js
@@ -14,49 +14,63 @@ async function getProductInfoById(productId) {
 
 export default async function ProductInfoPage({ params }) {
   const productId = params.productId;
-  const product = await getProductInfoById(productId);
-  const {
-    productName,
-    price,
-    imagesUrl,
-    quantity: availableQuantity,
-    description: productDescription,
-  } = product;
-  return (
-    <main className="md:max-w-[80vw] md:m-auto">
-      <h1 className="text-2xl md:mb-5">{productName}</h1>
-      <div className="bg-white min-h-[300px]">
-        {/* image Carousel */}
-        <div className="bg-white rounded max-w-lg m-auto">
-          <Carousel>
-            {imagesUrl.map((image) => (
-              <img src={image} alt={productName} />
-            ))}
-          </Carousel>
+  try {
+    const product = await getProductInfoById(productId);
+    const {
+      productName,
+      price,
+      imagesUrl,
+      quantity: availableQuantity,
+      description: productDescription,
+    } = product;
+    return (
+      <main className="md:max-w-[80vw] md:m-auto">
+        <h1 className="text-2xl md:mb-5">{productName}</h1>
+        <div className="bg-white min-h-[300px]">
+          {/* image Carousel */}
+          <div className="bg-white rounded max-w-lg m-auto">
+            <Carousel>
+              {imagesUrl.map((image) => (
+                <img src={image} alt={productName} />
+              ))}
+            </Carousel>
+          </div>
         </div>
-      </div>
 
-      {/* price, and action buttons */}
-      <div className="flex justify-evenly mt-5 mb-5 sm:wrap">
-        <h2>
-          <span className="text-2xl font-bold">K{price}</span> per Qty
-        </h2>
-        {/* check if the product is still in stock before
+        {/* price, and action buttons */}
+        <div className="flex justify-evenly mt-5 mb-5 sm:wrap">
+          <h2>
+            <span className="text-2xl font-bold">K{price}</span> per Qty
+          </h2>
+          {/* check if the product is still in stock before
         showing the BuyNow button and AddToCartButton
         */}
-        {availableQuantity == 0 ? (
-          <p className="text-red-700">
-            We are out of stock. Item No longer Available
-          </p>
-        ) : (
-          <>
-            <BuyNowButton productId={productId} />
-            <AddToCartButton productId={productId} productName={productName} />
-          </>
-        )}
+          {availableQuantity == 0 ? (
+            <p className="text-red-700">
+              We are out of stock. Item No longer Available
+            </p>
+          ) : (
+            <>
+              <BuyNowButton productId={productId} />
+              <AddToCartButton
+                productId={productId}
+                productName={productName}
+              />
+            </>
+          )}
+        </div>
+        {/* product descriptions*/}
+        <p>{productDescription}</p>
+      </main>
+    );
+  } catch (e) {
+    return (
+      <div className="h-full flex flex-col justify-center items-center">
+        <p className="text-blue-600 text-xl">
+          Sorry! something went wrong. Try refreshing the page
+        </p>
+        <p>Also do check your internet connection.</p>
       </div>
-      {/* product descriptions*/}
-      <p>{productDescription}</p>
-    </main>
-  );
+    );
+  }
 }
